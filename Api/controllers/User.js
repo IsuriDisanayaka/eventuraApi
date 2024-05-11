e57623e7177a1d66e51ce52ff7d72628ff32ddef
Isuri Disanayaka <isuriUmeshika1@gmail.com>
2024-05-11 20:05:37 +0530

    google authentication

---
+++ Api/controllers/User.js
@@ -0,0 +1,22 @@
+// Assuming this is in your userController.js
+const User = require("../models/User");
+
+exports.handleGoogleLogin = async (req, res) => {
+  const { googleId, email, firstName, lastName, picture } = req.body;
+  try {
+    let user = await User.findOne({ googleId });
+    if (!user) {
+      user = new User({
+        googleId,
+        email,
+        firstName,
+        lastName,
+        picture,
+      });
+      await user.save();
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error", details: error });
+  }
+};
--- Api/db/server.js
+++ Api/db/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const express = require("express");
 const connectDB = require("./db");
 const eventRoutes = require("../routes/Event");
+const userRoutes = require("../routes/User");
 
 const PORT = process.env.PORT;
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(eventRoutes);
+app.use(userRoutes);
 connectDB();
 
 const server = app.listen(PORT, () => {
